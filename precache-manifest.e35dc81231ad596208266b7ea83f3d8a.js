fc300bf12e931cef7cf42aac4c252f10887752dd
Chanuga <Chanugatharindu1995@gmail.com>
2020-03-30 11:33:07 +0530

    Updates

---
+++ precache-manifest.e35dc81231ad596208266b7ea83f3d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8636ee848acdf4eb0c835fda3bf31e22",
+    "url": "/Chanuga/covid19.git/index.html"
+  },
+  {
+    "revision": "d07a567b6c51b3834819",
+    "url": "/Chanuga/covid19.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e8113e740cf3e72beede",
+    "url": "/Chanuga/covid19.git/static/js/2.60d1bc19.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Chanuga/covid19.git/static/js/2.60d1bc19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07a567b6c51b3834819",
+    "url": "/Chanuga/covid19.git/static/js/main.b749df75.chunk.js"
+  },
+  {
+    "revision": "789587988930c1af55f4",
+    "url": "/Chanuga/covid19.git/static/js/runtime-main.90fa2643.js"
+  }
+]);
